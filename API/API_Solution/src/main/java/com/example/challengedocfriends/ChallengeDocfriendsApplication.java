0f923f2832dbf7a3119dfc1024837c229e31fd96
chunbong <cdy0266@gmail.com>
2019-10-09 01:45:46 +0900

    프로젝트 생성

---
+++ API/API_Solution/src/main/java/com/example/challengedocfriends/ChallengeDocfriendsApplication.java
@@ -0,0 +1,13 @@
+package com.example.challengedocfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeDocfriendsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeDocfriendsApplication.class, args);
+    }
+
+}
