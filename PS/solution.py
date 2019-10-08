4ee272d8f78e2a95199e747925776e358319ac64
chunbong <cdy0266@gmail.com>
2019-10-08 12:34:54 +0900

    [수정] 숫자 배열에서 작은 수, 큰 수를 탐색

--- PS/solution.py
+++ PS/solution.py
@@ -1,20 +1,22 @@
-q = input()
-
 '''
 가장 작은 수와 가장 큰 수가 여러번 나오는데 
 어떤 기준으로 a번째, b번째가 정해지는지 명확하지 않아서
-가장 작은 수와 가장 큰 수의 위치를 모두 저장하고
-a, b를 입력 받아서 가장 작은 수의 인덱스, 가장 큰 수의 인덱스 일 때 정답을 출력하도록 했습니다
+가장 작은 수와 가장 큰 수의 위치를 모두 저장하고,
+저장된 위치를 출력, a, b를 선택하도록 했습니다.
 '''
-a, b = map(int, input().split())
+
+q = input()
 
 numbers = []
 minNumIndex = []
 maxNumIndex = []
-for i in range(len(q)):
+
+for i in range(len(q)):     # 전체 문자열에서 숫자 배열을 생성
     if "0" <= q[i] <= "9":
         numbers.append(q[i])
-    if q[i] == "0":
+
+for i in range(len(numbers)):   # 숫자 배열에서 가장 작은 수(0)과 가장 큰 수(9)의 인덱스들을 저장
+    if numbers[i] == "0":
         minNumIndex.append(i)
-    if q[i] == "9":
+    if numbers[i] == "9":
         maxNumIndex.append(i)
